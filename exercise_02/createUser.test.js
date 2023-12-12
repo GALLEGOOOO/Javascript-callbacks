df44e4afefa0ec1efe03567a792a2a89440ab9dc
DESKTOP-NGJO7RE\arnau <arnau.gallego@mataro.epiaedu.cat>
2023-12-12 12:42:58 +0100

    test ex2 fixed and formated

--- exercise_02/createUser.test.js
+++ exercise_02/createUser.test.js
@@ -17,18 +17,18 @@ describe("Given function createUser", () => {
 
 describe("Given function createUser", () => {
   describe("When function is called with an email as an empty string", () => {
-    test("Then a string is returned for success case", async () => {
+    test("Then a string is returned for success case", () => {
       // Arrange
       const inputString = "email@gmail.com";
+
       // Act
-      const result = await createUser(inputString, onError, onSuccess);
+      const result = createUser(inputString, onError, onSuccess);
+
       // Assert
-      expect(result).toBe(
+      expect(result).toEqual(
         `User with email: ${inputString} has been correctly created` ||
           "Error: user not created",
       );
     });
   });
 });
-
-describe("Given function createUser", () => {});
